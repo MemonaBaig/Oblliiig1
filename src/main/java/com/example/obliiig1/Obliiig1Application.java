d732e5b600e97778339ec2f25de8990da7643621
Memona_Baig <143513798+MemonaBaig@users.noreply.github.com>
2024-02-14 15:49:15 +0100

    vet ikke 2

---
+++ src/main/java/com/example/obliiig1/Obliiig1Application.java
@@ -0,0 +1,13 @@
+package com.example.obliiig1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Obliiig1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Obliiig1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/obliiig1/Obliiig1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obliiig1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Obliiig1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
